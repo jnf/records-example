6ccb100c7a966bbf97f4435b78a9685895337367
jnf <jeremy.flores@gmail.com>
2015-07-05 14:51:58 -0700

    Complete the basic browsing experience by making unscoped albums browsable.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,9 @@
 class AlbumsController < ApplicationController
+  def index
+    @albums = Album.includes(:artist).order(:title)
+  end
+
+  def show
+    @album = Album.includes(:artist).find(params[:id])
+  end
 end
