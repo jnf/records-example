57003bedb346a251a52d4296d56fd3ce8b4c450e
jnf <jeremy.flores@gmail.com>
2015-07-05 14:44:53 -0700

    Add/tweak views for exploring by Artist unscoped by Label.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,8 @@
 class ArtistsController < ApplicationController
+  def index
+    @artists = Artist.order(:name)
+  end
+
   def show
     @label  = params[:label_id] && Label.find(params[:label_id])
     @artist = Artist.includes(:albums).find(params[:id])
