9bfe0cca8cf1fde03daf650737f56adf4ff68c7c
jnf <jeremy.flores@gmail.com>
2015-07-05 22:05:03 -0700

    Sample validations for discussion.

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,14 @@
 class Album < ActiveRecord::Base
+  # Associations ---------------------------------------------------------------
   belongs_to :artist
   belongs_to :label
 
+  # Validations ----------------------------------------------------------------
+  # validates :title, presence: true
+  # validates :released_year, presence: true, numericality: true
+  # validates :format, presence: true
+  # validates :label_code, presence: true
+
+  # Scopes ---------------------------------------------------------------------
   scope :on, -> (label) { where(label: label) }
 end
