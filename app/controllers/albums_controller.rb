74011840aa8fa389e9c282cb2f31437065fd35de
jnf <jeremy.flores@gmail.com>
2015-07-05 21:46:15 -0700

    Adds an album creation workflow: needs artist and label associations & validations.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -6,4 +6,23 @@ class AlbumsController < ApplicationController
   def show
     @album = Album.includes(:artist).find(params[:id])
   end
+
+  def new
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.new(album_creation_params)
+    if @album.save
+      redirect_to album_path(@album)
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def album_creation_params
+    params.require(:album).permit(:title, :label_code, :format, :released_year)
+  end
 end
