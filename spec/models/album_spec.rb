a819957c41e75249f8a2c1358516a42ffaa879f2
jnf <jeremy.flores@gmail.com>
2015-07-07 11:10:09 -0700

    Adds tests for Album scopes.

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -27,4 +27,43 @@ RSpec.describe Album, type: :model do
       end
     end
   end
+
+  describe "available_formats scope" do
+    # positive test - includes unique formats
+    it "has all the unique formats in alphabetical order" do
+      album1 = Album.create(title: 'a', released_year: 1999, label_code: 'l', format: 'a format')
+      album3 = Album.create(title: 'a', released_year: 1999, label_code: 'l', format: 'c format')
+      album2 = Album.create(title: 'a', released_year: 1999, label_code: 'l', format: 'b format')
+
+      correct_order = [album1.format, album2.format, album3.format]
+      expect(Album.available_formats).to eq correct_order
+    end
+
+    # negative test - excludes duplicates formats
+    it "excludes duplicate formats" do
+      album1 = Album.create(title: 'a', released_year: 1999, label_code: 'l', format: 'a format')
+      album3 = Album.create(title: 'a', released_year: 1999, label_code: 'l', format: 'a format')
+      album2 = Album.create(title: 'a', released_year: 1999, label_code: 'l', format: 'b format')
+
+      correct_order = [album1.format, album2.format]
+      expect(Album.available_formats).to eq correct_order
+    end
+  end
+
+  describe "on scope" do
+    before :each do
+      @label = Label.create(title: 'Fancy Label')
+      Album.create(label: @label, title: 'a', released_year: 1999, label_code: 'l', format: 'a format')
+      Album.create(label: @label, title: 'b', released_year: 1999, label_code: 'l', format: 'a format')
+      Album.create(label: @label, title: 'c', released_year: 1999, label_code: 'l', format: 'a format')
+
+      some_other_label = Label.create(title: 'Unfancy Label')
+      @excluded_album = Album.create(label: some_other_label, title: 'd', released_year: 1999, label_code: 'l', format: 'a format')
+    end
+
+    it "includes only the records on the provided label" do      
+      expect(Album.on(@label).count).to eq 3
+      expect(Album.on(@label)).to_not include(@excluded_album)
+    end
+  end
 end
