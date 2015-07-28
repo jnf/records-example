c0a1dc85a6b80d207ee9400d90031c85162a350d
jnf <jeremy.flores@gmail.com>
2015-07-28 09:46:36 -0700

    Migrates the db to include an image column for Album.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150622050437) do
+ActiveRecord::Schema.define(version: 20150728163313) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150622050437) do
     t.string   "label_code"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "image"
   end
 
   create_table "artists", force: :cascade do |t|
